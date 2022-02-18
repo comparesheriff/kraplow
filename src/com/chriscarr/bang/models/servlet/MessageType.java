f954122da6b24b86d1e9babfa97af9b7f249d797
CompareSheriff <comparesheriff@gmx.net>
2022-02-18 21:55:30 +0100

    replace magic strings with enum MessageType

---
+++ src/com/chriscarr/bang/models/servlet/MessageType.java
@@ -0,0 +1,19 @@
+package com.chriscarr.bang.models.servlet;
+
+public enum MessageType {
+    GETGAMESTATE,
+    JOIN,
+    JOINAI,
+    LEAVE,
+    COUNTPLAYERS,
+    GETGUESTCOUNTER,
+    AVAILABLEGAMES,
+    CANSTART,
+    CHAT,
+    GETCHAT,
+    START,
+    CREATE,
+    GETMESSAGE,
+    SENDRESPONSE,
+    GETPLAYERINFO,
+}
